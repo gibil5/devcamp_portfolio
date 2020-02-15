3f18b0f43e627495269e27030bb1b1dfb5e476ce
Javier Revilla <jrevilla.pcm@gmail.com>
2020-02-15 11:16:20 -0500

    Added portfolio actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,8 +1,28 @@
 class PortfoliosController < ApplicationController
 
   def index
-    @portfolio_items = Portfolio.all 
+    @portfolio_items = Portfolio.all
   end
 
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 
 end
