46925e2c0b52f67340b07ca65d125a736b646f6d
Javier Revilla <jrevilla.pcm@gmail.com>
2020-02-27 18:40:55 -0500

    Models - Relationships

---
+++ db/migrate/20200227232643_add_topic_reference_to_blogs.rb
@@ -0,0 +1,6 @@
+class AddTopicReferenceToBlogs < ActiveRecord::Migration[6.0]
+  def change
+    #add_reference :blogs, :topic, null: false, foreign_key: true
+    add_reference :blogs, :topic, foreign_key: true
+  end
+end
