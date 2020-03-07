aa868d6c4ed91948b89818205a3b0d1c1f1a17f8
Javier Revilla <jrevilla.pcm@gmail.com>
2020-03-07 12:42:14 -0500

    Implemented image defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,6 +7,20 @@ class Portfolio < ApplicationRecord
     where(subtitle: "Angular")
   end
 
-  # scopes - method - a lambda 
+  # scopes - method - a lambda
   scope :ruby_on_rails_items, -> {where(subtitle: "Ruby on Rails")}
+
+  # When the new action is called
+  after_initialize :set_defaults
+  #after_create :set_defaults    # not the right spot to set defaults
+
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/600x200"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end
+
+  # Double pipes - Shortcut for
+  if self.main_image == nil
+    self.main_image = "https://via.placeholder.com/600x200"
+  end
 end
