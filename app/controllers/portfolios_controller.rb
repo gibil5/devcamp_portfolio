226744c1686c2d5774a6a660268bc7c621006c39
Javier Revilla <jrevilla.pcm@gmail.com>
2020-03-21 16:32:58 -0500

    Cleaning

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,11 +37,9 @@ class PortfoliosController < ApplicationController
     end
   end
 
-
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
-
 
   def destroy
     @portfolio_item = Portfolio.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,10 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'tails', to: 'pages#tails'
-
   resources :blogs do
     member do
       get :toggle_status
     end
   end
-
   root to: 'pages#home'
 end
