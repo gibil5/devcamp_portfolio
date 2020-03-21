216310c923731635775dbd2ca9d75866c791f553
Javier Revilla <jrevilla.pcm@gmail.com>
2020-03-21 16:46:01 -0500

    Refactored for concerns

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -2,7 +2,6 @@ module DeviseWhitelist
   extend ActiveSupport::Concern
 
   included do
-    #before_filter :configure_permitted_params, if: :devise_controller?   # Dep in Rails 6 
     before_action :configure_permitted_params, if: :devise_controller?
   end
 
